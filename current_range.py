183191839ddf14906904e28bd618e2d5fd37c213
hus6kor <sunilgowda95@gmail.com>
2023-01-23 14:24:07 +0530

    adding small code to pass test case 02

--- current_range.py
+++ current_range.py
@@ -1,2 +1,6 @@
+def get_unique_list(input):
+    return list(set(input))
+
 def get_range(current_samples):
-    return "4-5, 2"
+    unique_current_sample = get_unique_list(current_samples)
+    return "{}-{}, {}".format(unique_current_sample[0], unique_current_sample[-1], len(current_samples))

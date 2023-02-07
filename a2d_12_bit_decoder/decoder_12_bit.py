c46890ad74d200e5de7976be0a94502dd8f42bc5
hus6kor <sunilgowda95@gmail.com>
2023-02-07 16:26:51 +0530

    integrating with continuous range finder function

--- a2d_12_bit_decoder/decoder_12_bit.py
+++ a2d_12_bit_decoder/decoder_12_bit.py
@@ -12,7 +12,12 @@ sensor_details_12bit = {
     }
 }
 
-def decode_12bit(raw_digital_values):
+def decode_12_bit(raw_digital_values):
     raw_values_in_range = ignore_out_of_bound(sensor_details_12bit, raw_digital_values)
     return convert_to_amps(sensor_details_12bit, raw_values_in_range)
+
+def decode_12_bit_continous_range(raw_digital_values):
+    current_values = decode_12_bit(raw_digital_values)
+    from continuous_range.current_range import get_range
+    return get_range(current_values)
     
--- a2d_decoder.test.py
+++ a2d_decoder.test.py
@@ -4,7 +4,7 @@ import os
 
 class TypewiseTest(unittest.TestCase):
     def test_decoder(self):
-        from test_code.t_decoder import test
+        from test_code.t_decoder_12_bit import test
         test()
             
 if __name__ == '__main__':
